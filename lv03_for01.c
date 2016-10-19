e7915c8b25400d38ecfee20cf49545fb93966212
DiepHan <diephandh97@gmail.com>
2016-10-19 23:07:18 +0700

    Add files via upload

---
+++ lv03_for01.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int n,i;
+    float s;
+    printf("Nhap n so dau tien:");
+    scanf("%d",&n);
+    s=0;
+    for(i=1;i<=n;i++)
+        s=s+(float)1/i;
+    printf("S=%0.2f",s);
+
+}
